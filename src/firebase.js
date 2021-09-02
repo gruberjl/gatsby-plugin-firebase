b188147483e316d27775c4062e3f65be34fc9ba9
John Gruber <gruberjl@gmail.com>
2021-09-02 16:59:12 -0400

    updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
-import firebase from "firebase/app";
+import {initializeApp} from "firebase/app";
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
+let firebase
+
+if (!firebase) {
+  firebase = initializeApp({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
